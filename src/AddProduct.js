494ee8d2d0aa8a55adfa069b7f35925099cab122
nykang17 <61221081+mikanjuice@users.noreply.github.com>
2024-02-15 12:33:37 -0800

    Adding AddProduct.js (the add product page) via upload
    First draft of the add product page which allows users to add products through a form to our firebase database and storage. I've added the link to the Add Product page to our Navbar within our home page.

---
+++ src/AddProduct.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { storage, db } from "../config/Config";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { collection, addDoc } from "firebase/firestore";
+import { v4 } from "uuid";
+
+export const AddProduct = () => {
+
+    const[name, setName] = useState('');
+    const[description, setDescription] = useState('');
+    const[price, setPrice] = useState('');
+    const[image, setImage] = useState(null);
+
+    const[imageError, setImageError] = useState('');
+    const[successMsg, setSuccessMsg] = useState('');
+    const[uploadError, setUploadError] = useState('');
+    
+    const imgTypes = ['image/png', 'image/PNG', 'image/jpg', 'image/jpeg']
+    const handleProductImg = (e) => {
+        let selectedFile = e.target.files[0];
+        if(selectedFile){
+            if(selectedFile && imgTypes.includes(selectedFile.type)){
+                setImage(selectedFile);
+                setImageError('');
+            }
+            else {
+                setImage(null);
+                setImageError('Please select a valid image file type (jpg or png)')
+            }
+        }
+        else{
+            console.log('Please select your file');
+        }
+    }
+
+    const handleAddProduct = (e) => {
+        //prevent refreshing the page 
+        e.preventDefault();
+
+        // we store the image name in our firebase storage in folder product images (for ease we put that task in a function)
+        const storageRef = ref(storage, `product-images/${image.name + v4()}`);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        // store the product itself in our firestore database
+        const colRef = collection(db, 'products');
+
+        // add the image to our storage and show the progress by bytes, if error, then set an upload error
+        uploadTask.on('state_changed',snapshot=>{
+            const progress = (snapshot.bytesTransferred/snapshot.totalBytes)*100
+            console.log(progress);
+        },error=>setUploadError(error.message),()=>{ // after that, get the URL of the image, and then add the product into our database
+            getDownloadURL(uploadTask.snapshot.ref).then(url=>{
+                addDoc(colRef, {
+                    name,
+                    description,
+                    price: Number(price),
+                    url
+                }).then(()=>{ // if it was successful, then say it was so, and then reset the form
+                    setSuccessMsg('Product added succesfully');
+                    setName('');
+                    setDescription('');
+                    setPrice('');
+                    document.getElementById('file').value='';
+                    setImageError('');
+                    setUploadError('');
+                    setTimeout(()=>{
+                        setSuccessMsg('')
+                    },3000)
+                }).catch(error=>setUploadError(error.message));
+            })
+        })
+    }
+
+    return (
+        <div className='container'>
+            <br/>
+            <h2>Add your product</h2>
+            <hr/>
+            {successMsg&&<>
+                <div className='success-msg'>{successMsg}</div>
+                <br/>
+            </>}
+            <form autoComplete="off" className="form-group" onSubmit={handleAddProduct}>
+                <label>Product Name</label>
+                <br/>
+                <input type="text" className='form-control' required onChange={(e)=>setName(e.target.value)} value={name} />
+                <br/>
+                <label>Product Description</label>
+                <br/>
+                <input type="text" className='form-control' required onChange={(e)=>setDescription(e.target.value)} value={description} />
+                <br/>
+                <label>Product Price</label>
+                <br/>
+                <input type="number" className='form-control' required onChange={(e)=>setPrice(e.target.value)} value={price} />
+                <br/>
+                <label>Product Image</label>
+                <br/>
+                <input type="file" id='file' className='form-control' onChange={handleProductImg} />
+                {imageError&&<>
+                    <br/>
+                    <div className='error-msg'>{imageError}</div>
+                </>}
+                <br/>
+                <button type = "submit">ADD</button>
+            </form>
+            {uploadError&&<>
+                    <br/>
+                    <div className='error-msg'>{imageError}</div>
+                </>}
+        </div>
+    )
+}
