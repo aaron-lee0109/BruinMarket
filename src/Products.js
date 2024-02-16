28364b18003bdf8d2e79cf6f37d9ea59e9788693
nykang17 <61221081+mikanjuice@users.noreply.github.com>
2024-02-15 23:31:29 -0800

    Adding Products.js into our src folder
    The Products.js file contains code for displaying all the products users have uploaded onto our website.

--- src/Products.js
+++ src/Products.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-export const Products = () => {
-    return (
-        <div>
-            Products
+export const Products = ({products}) => {
+    
+    // using the products array we were passed, map those products one by one and display their image, name and price
+    return products.map(item =>(
+        <div key = {item.id} className='product'>
+            <div className='product-img'>
+                <img src={item.url} alt='product-img' />
+            </div>
+            <div className='product-name'>{item.name}</div>
+            <div className='product-price'>{item.price}</div>
         </div>
-    )
+    ))
 }
