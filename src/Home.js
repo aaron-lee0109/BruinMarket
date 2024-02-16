a4a016a3b23192e60d29ce9ec41148112d3f6694
nykang17 <61221081+mikanjuice@users.noreply.github.com>
2024-02-15 23:29:49 -0800

    Displaying the products uploaded by users in homepage
    I've added the code necessary to display the products uploaded by users on our homepage, so then users can see what's available on the market. The actual part of the code that takes each item and displays their image, name and price are in the Products.js file. I might move the entire section where I get each item in the database and store it in an array, into the Products.js folder to make this file clean as possible.

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import { Navbar } from './Navbar'
 import { Products } from './Products'
+import { db } from "../config/Config";
+import { collection, getDocs } from "firebase/firestore";
 
 export const Home = () => {
+    // create the array of products we're going to display
+    const[products, setProducts] = useState([]);
+
+    // create a function that will retrieve all the products in our database, and store them in our array
+    const getProducts = async () => {
+        const querySnapshot = await getDocs(collection(db, 'products'));
+        const productsArray = [];
+        querySnapshot.forEach((doc) => {
+            productsArray.push({ ...doc.data(), id: doc.id });
+        });
+        setProducts(productsArray);
+    } 
+    useEffect(() => {
+        getProducts();
+    },[])
+
     return (
         <div>
             <Navbar />
-            <Products />
+            <br/>
+            {products.length > 0 && (       // if we have at least 1 product, then display those products on our homepage
+                <div className='container-fluid'>
+                    <h1 className='text-center'>Products</h1>
+                    <div className='products-box'>
+                        <Products products={products} />
+                    </div>
+                </div>
+            )}
+            {products.length < 1 && (      // if we don't have any or loading, then display "please wait"
+                <div className='container-fluid'>
+                    Please wait...
+                </div>
+            )}
         </div>
     )
 }
